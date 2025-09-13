11e9c309fce618902e5fdbf3eb25fc42f6d58110
VuTrungNghia <vunghia467@gmail.com>
2025-09-13 20:32:12 +0700

    Create Sample categories data temporary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 // import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home/home'; 
+import Home from './components/Home/home';
+import Categories from './components/Categories/categories';
+import Statistics from './components/Statistics/statistics';
+import Settings from './components/Settings/settings';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 function App() {
@@ -12,6 +15,9 @@ function App() {
           <main className="flex-1">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/Categories" element={<Categories />} />
+              <Route path="/Statistics" element={<Statistics />} />
+              <Route path="/Settings" element={<Settings />} />
             </Routes>
           </main>
           <Footer />
